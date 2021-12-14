1ae6d5407d19ac8cfe17b17b1633e1b351aa4dc2
LTKillong <wjs453832920@163.com>
2021-12-15 04:22:54 +0800

    head and foot3

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <!--    Todo:content-->
     <div style="width: 100px;height: 100px;"></div>
     <div style="width: 100px;height: 100px;"></div>
+    <div style="width: 100px;height: 100px;"></div>
     <!--    底部-->
     <my-foot></my-foot>
 </template>
